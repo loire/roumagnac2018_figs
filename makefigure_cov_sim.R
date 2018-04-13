e1b8549a5769a946813e7ec755072d7285cffc77
loire <loire@elitebook.localdomain>
2018-04-13 11:48:57 +0200

    Comment codes

--- makefigure_cov_sim.R
+++ makefigure_cov_sim.R
@@ -1,24 +1,16 @@
+# get data
 data = read.table("Final.data")
 colnames(data) = c("type","pos","coverage","match","similarity")
-data %>% select(-match)
 require(tidyverse)
 
+
+# First plot raw results:
 svg("Brut_results.svg")
 data %>% select(- match ) %>%  gather(variable, value, 3:4) %>% ggplot(aes(x=pos,y=value,group = variable)) + 
   geom_line() + facet_grid(variable ~ type ,scale="free_y") + theme_bw() + xlab("position")
 dev.off()
 
-
-data %>% mutate(similarity=ifelse(coverage<1,NA,similarity)) %>% group_by(type) %>% summarize(n=mean(similarity,na.rm=T))
-
-
-data %>% rowwise %>% mutate(similarity=ifelse(coverage<100,NA,similarity)) %>% 
-  select(- match ) %>%  gather(variable, value, 3:4) %>% ggplot(aes(x=pos,y=value,group = variable)) + 
-  geom_line() + 
-  facet_grid(variable ~ type ,scale="free") + theme_bw() + xlab("position")
-
-dev.off()
-
+# Plot both coverage on the same fig:
 svg("Coverage.svg",width=12)
 data %>% select(- match ) %>%  gather(variable, value, 3:4) %>% 
   filter(variable=="coverage") %>%
@@ -27,7 +19,7 @@ data %>% select(- match ) %>%  gather(variable, value, 3:4) %>%
   scale_color_brewer(type="qual",palette="Set1") + theme(text=element_text(size=16,family="Times"))
 dev.off()
 
-
+# Tried log but it sucks
 svg("similarity_log.svg")
 data %>% select(- match ) %>%  gather(variable, value, 3:4) %>% 
   filter(variable=="similarity")  %>%
@@ -36,31 +28,30 @@ data %>% select(- match ) %>%  gather(variable, value, 3:4) %>%
   scale_color_brewer(type="qual",palette="Set1") 
 dev.off()
 
+# Using zoo to compute sliding windows
 require(zoo)
 
-data %>% filter(type=="minion") %>% select(similarity) %>% rollmean(.,10,na.omit) 
+# Start with a 100bp windows size)
 
+# Create subdatasets for each sequencing:
 minionsim = rollapply(data %>% filter(type=="minion") %>% select(similarity), width = 100, by = 1, FUN = mean, align = "center")
 illusim = rollapply(data %>% filter(type=="illumina") %>% select(similarity), width = 100, by = 1, FUN = mean, align = "center")
-
 minionsimdf = data.frame("pos"=seq(1:length(minionsim)),"type"=rep("minion",length(minionsim)),"similariy"=minionsim)
 illusimdf = data.frame("pos"= seq(1:length(illusim)),"type"=rep("illumina",length(illusim)),"similariy"=illusim)
 datasim = rbind(illusimdf,minionsimdf)
-
+# Draw fig
 svg("similarity_sliding100.svg",width=12)
 datasim %>% ggplot() + geom_line(aes(x=pos,y=similarity,group=type,color =type),size = 0.8,alpha=0.7) + 
   theme_bw() + ylab("similarity") + xlab("position") + 
   scale_color_brewer(type="qual",palette="Set1") + theme(text=element_text(size=16,family="Times"))
 dev.off()
 
-
+# Same for 10 bp
 minionsim = rollapply(data %>% filter(type=="minion") %>% select(similarity), width = 10, by = 1, FUN = mean, align = "center")
 illusim = rollapply(data %>% filter(type=="illumina") %>% select(similarity), width = 10, by = 1, FUN = mean, align = "center")
-
 minionsimdf = data.frame("pos"=seq(1:length(minionsim)),"type"=rep("minion",length(minionsim)),"similariy"=minionsim)
 illusimdf = data.frame("pos"= seq(1:length(illusim)),"type"=rep("illumina",length(illusim)),"similariy"=illusim)
 datasim = rbind(illusimdf,minionsimdf)
-
 svg("similarity_sliding10.svg",width=12)
 datasim %>% ggplot() + geom_line(aes(x=pos,y=similarity,group=type,color =type),size = 0.8,alpha=0.7) + 
   theme_bw() + ylab("similarity") + xlab("position") + 
@@ -68,25 +59,16 @@ datasim %>% ggplot() + geom_line(aes(x=pos,y=similarity,group=type,color =type),
 dev.off()
 
 
-
+# Same for 50bp
 minionsim = rollapply(data %>% filter(type=="minion") %>% select(similarity), width = 50, by = 1, FUN = mean, align = "center")
 illusim = rollapply(data %>% filter(type=="illumina") %>% select(similarity), width = 50, by = 1, FUN = mean, align = "center")
-
 minionsimdf = data.frame("pos"=seq(1:length(minionsim)),"type"=rep("minion",length(minionsim)),"similariy"=minionsim)
 illusimdf = data.frame("pos"= seq(1:length(illusim)),"type"=rep("illumina",length(illusim)),"similariy"=illusim)
 datasim = rbind(illusimdf,minionsimdf)+ theme(text=element_text(size=16,family="Times"))
-
 svg("similarity_sliding50.svg",width=12)
 datasim %>% ggplot() + geom_line(aes(x=pos,y=similarity,group=type,color =type),size = 0.8,alpha=0.7) + 
   theme_bw() + ylab("similarity") + xlab("position") + 
   scale_color_brewer(type="qual",palette="Set1") 
 dev.off()
 
-
-
-
-
-
-
-
-
+# That's it. Inkscaped the best figures
--- treatmpilup.py
+++ treatmpilup.py
@@ -1,10 +1,12 @@
 import sys
+# Read mpileup file and extract sequencing type by the name
 name = sys.argv[1].split(".")[0]
 with open(sys.argv[1],'r') as f:
     for line in f:
         data = line.split()
         pos = data[1]
         cov = data[3]
+        # Count number of match on forward (.) and reverse (,) to compute similarity
         if int(cov) > 0:
             match = data[4].count(",")
             match += data[4].count(".")
